3027779b3bb41feeee123211c18b798b8fc7eda0
Elfesya ESEN <49937779+2905060048@users.noreply.github.com>
2022-03-10 12:49:51 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,19 @@
 <?php
+session_start(); ob_start();
+$host = 'localhost';
+$dbname   = 'test';
+$username = 'root';
+$password = '';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $username, $password, $options);
+} catch (\PDOException $e) {
+     die($e->getMessage());
+}
