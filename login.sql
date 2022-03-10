8ae7ddd84fb8bdd328fd36024be3e94249fd8fe4
Elfesya ESEN <49937779+2905060048@users.noreply.github.com>
2022-03-10 13:21:24 +0300

    Update login.sql

--- login.sql
+++ login.sql
@@ -1,1 +1,9 @@
 
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `email` varchar(96) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
