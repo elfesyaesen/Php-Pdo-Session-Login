5ba71832e642a4b73816f69b4d4f814dee7e50c6
Elfesya ESEN <49937779+2905060048@users.noreply.github.com>
2022-03-10 13:15:47 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,8 @@
-<?php
+<?php session_start(); ob_start();
+unset($_SESSION['user_id']);
+unset($_SESSION['email']);
+unset($_SESSION['firstname']);
+unset($_SESSION['lastname']);
+//or
+session_destroy();
+header('location: login.php');
