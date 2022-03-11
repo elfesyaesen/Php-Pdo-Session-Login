b4378feb9c50a168e5f0e3d48095bb3d3ec58415
Elfesya ESEN <49937779+2905060048@users.noreply.github.com>
2022-03-11 13:01:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php 
   include_once 'db.php';
-  if(isset($_SESSION['user_id'])){ print 'Welcome :'.$_SESSION['firstname']; } else { header('location: login.php'); }
+  if(isset($_SESSION['user_id'])){ print 'Welcome :'.$_SESSION['firstname'].'<br><a href="logout.php">Oturumu Kapat</a>';  } else { header('location: login.php'); }
